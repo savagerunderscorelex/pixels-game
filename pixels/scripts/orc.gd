a9c0ab9a2c966a62144090a1ff6c3a0562977aab
savagerunderscorelex <eigbekoyi@gmail.com>
2025-12-09 06:29:36 -0500

    adjusted size of orc; created separate hurtbox for character; started on code for hitbox logic for the orc

--- pixels/scripts/orc.gd
+++ pixels/scripts/orc.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 # Issue Notes: 
 # Fixed Orc sitting on player issue by disabling floor layer 1 and setting on leave to do nothing in CharacterBody2D node
+
+@onready var animator = $Sprite2D
+@onready var attacker = $AnimationPlayer
+@onready var hitbox = $AxeHitbox
+
 var CHARACTER_SPEED: int = 150
 var isAttacking = false
-@onready var animator = $Sprite2D
 var player = null
-var getting2Close = false
 
 func _ready() -> void:
-	pass
+	hitbox.add_to_group("Enemy Hitboxes")
 
 func _physics_process(delta: float) -> void:
 	update_animation()
@@ -19,10 +22,7 @@ func _physics_process(delta: float) -> void:
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		player = body
-	if getting2Close == true:
-		print("too close now!") # debug :(
-		velocity = Vector2(0,0)
-		animator.play("attack")
+
 		
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	match body.name: # Match statement, switch in other languages (just found out about them, wanted to try it out :3)
@@ -30,24 +30,36 @@ func _on_player_detection_body_exited(body: Node2D) -> void:
 			player = null
 
 func flip_self():
-	if player.position.x < 0:
+	if player.position.x < 0 && !isAttacking:
 		animator.flip_h = true
 	else:
 		animator.flip_h = false
-	animator.play("walk")
 	
 func attack():
-	if player:
-		var distanceFromPlayer = global_position.distance_to(player.global_position)
-		if distanceFromPlayer < 100:
-			getting2Close = true
+	if isAttacking == true:
+		print("too close now!") # debug :(
+		velocity = velocity * 0	
 
 func update_animation():
 	if !isAttacking && velocity == Vector2(0,0):
 		animator.play("idle")
+	elif isAttacking == true:
+		attacker.play("attack")
+	else:
+		animator.play("walk")
 
 func move_to_player():
 	velocity = Vector2.ZERO
-	if player:
+	if player && isAttacking == false:
 		velocity = position.direction_to(player.position) * CHARACTER_SPEED
 		flip_self()
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		isAttacking = true
+		attack()
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		await get_tree().create_timer(1).timeout
+		isAttacking = false
