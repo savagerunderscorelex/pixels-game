32d26c2f15b5549d35142be724625ea60d805190
savagerunderscorelex <eigbekoyi@gmail.com>
2025-12-12 23:09:40 -0500

    removed level two scene; renamed health component scene to goals scene; finished level one and added logic + adjusted ui to be more like an actual game; duplicated and added rest of levels (level 4 and level 2),

--- pixels/scripts/health_component.gd
+++ pixels/scripts/health_component.gd
@@ -1,17 +1,34 @@
 extends Control
 
+# Watched a video about how statically typing kind of improves speed, thought it was a good habit to try out
+# I did do this when first starting out with Godot, but that was because the tutorial guys did that
 @onready var completeLevels : RichTextLabel = $RichTextLabel2
 @onready var defeatOrc : RichTextLabel = $RichTextLabel3
 @onready var dontDie : RichTextLabel = $RichTextLabel4
+@onready var button : Button = $Button
+
 
 func _ready() -> void:
 	completeLevels.visible = false
 	defeatOrc.visible = false
 	dontDie.visible = false
+	button.visible = false
+	button.disabled = true
 	
 func _process(delta: float) -> void:
 	show_goals()
 	
-func show_goals():
-	await get_tree().create_timer(4).timeout
+func show_goals(): # Showing each goal after 3.5 secs to match up with the music :3
+	await get_tree().create_timer(3.5).timeout
 	completeLevels.visible = true
+	await get_tree().create_timer(3.5).timeout
+	defeatOrc.visible = true
+	await get_tree().create_timer(3.5).timeout
+	dontDie.visible = true
+	
+func next_button_appear():
+	pass
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.
+#ac6963
--- pixels/scripts/level_one.gd
+++ pixels/scripts/level_one.gd
@@ -14,3 +14,9 @@ func _process(delta: float) -> void:
 func _on_right_first_panel_boundary_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		secondCamera.enabled = !secondCamera.enabled
+
+
+func _on_to_next_level_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_file("res://scenes/you_won.tscn")
